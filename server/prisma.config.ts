e0fefa28efdc053b723da5766efba2a1aff1f21e
txrunishere <tsoni9286@gmail.com>
2026-06-05 12:22:42 +0530

    feat: integrate Prisma with PostgreSQL for user and session management

---
+++ server/prisma.config.ts
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import { defineConfig } from "prisma/config";
+import { env } from "./src/config/env.config.js";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    url: env.DATABASE_URL,
+  },
+});
